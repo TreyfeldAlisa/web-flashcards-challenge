578f0ce5a8602c38184e6b495fed0f3dd0e7e937
WakaCappie <omniknight13@mail.ru>
2022-07-01 16:53:40 +0300

    onequest

--- views/Questions1.jsx
+++ views/Questions1.jsx
@@ -1,18 +1,18 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function Quest() {
+module.exports = function Quest1() {
   return (
     <Layout>
       <form>
         <h1> Вперед!</h1>
         <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">"Продолжите последовательность, Микеланджело, Леонардо, Рафаэль, ..."</label>
+          <label htmlFor="exampleFormControlInput1" className="form-label">"Устройство для измельчения бумаги и враг Черепашки-ниндзя:"</label>
         </div>
         <div className="mb-3">
           <label htmlFor="exampleFormControlTextarea1" className="form-label">Твой ответ</label>
           <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
-          <button className='enter-answer'><a href='http://localhost:3000/quest/1' >Отправить</a></button>
+          <button className='enter-answer'><a href='http://localhost:3000/quest/2' >Отправить</a></button>
         </div>
       </form>
     </Layout>
--- views/Questions4.jsx
+++ views/Questions4.jsx
@@ -1,18 +1,18 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function Quest() {
+module.exports = function Quest4() {
   return (
     <Layout>
       <form>
         <h1> Вперед!</h1>
         <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">"Продолжите последовательность, Микеланджело, Леонардо, Рафаэль, ..."</label>
+          <label htmlFor="exampleFormControlInput1" className="form-label">Мобильная танкоподобная крепость, которая впервые появился в мультсериале 1987 и использовался различными версиями Крэнга:</label>
         </div>
         <div className="mb-3">
           <label htmlFor="exampleFormControlTextarea1" className="form-label">Твой ответ</label>
           <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" />
-          <button className='enter-answer'><a href='http://localhost:3000/quest/1' >Отправить</a></button>
+          <button className='enter-answer'><a href='http://localhost:3000/quest/5' >Отправить</a></button>
         </div>
       </form>
     </Layout>
